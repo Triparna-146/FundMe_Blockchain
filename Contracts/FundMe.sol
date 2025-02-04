2f5954c2f6946d82157f2da15b80e9b8b8d811a9
Triparna-146 <kartriparna53@gmail.com>
2025-02-04 23:09:41 +0530

    withdraw function in the contract

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -11,7 +11,7 @@ contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUSD = 5 * 1e18;
+    uint256 public minimumUSD = 1 * 1e18;
 
     address[] public funders;
     mapping (address => uint256) public addressToAmountFunded;
@@ -21,7 +21,34 @@ contract FundMe {
         // Have a minimum limit $5
         require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough ETH");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] +=  msg.value;
+    }
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+
+    function withdraw () public onlyOwner {
+        
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        //Reseting the array    
+        funders = new address[](0);
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwner () {
+        require(msg.sender == owner, "Sender is not Owner");
+        _;
     }
     
 }
