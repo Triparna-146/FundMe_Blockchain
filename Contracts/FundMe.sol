2cd671f7682e12861d438505b313358d762a0e6b
Triparna-146 <kartriparna53@gmail.com>
2025-02-04 21:53:22 +0530

    made library for price converter

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -5,9 +5,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+
+    using PriceConverter for uint256;
 
     uint256 public minimumUSD = 5 * 1e18;
 
@@ -17,24 +19,9 @@ contract FundMe {
     function fund() public payable  {
         //Allow users to send money
         // Have a minimum limit $5
-        require(getConversionRate(msg.value) >= minimumUSD, "Didn't send enough ETH");
+        require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-    }
-
-    function getPrice () public view returns (uint256){
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        //Price of ETH in USD
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate (uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
     }
     
 }
